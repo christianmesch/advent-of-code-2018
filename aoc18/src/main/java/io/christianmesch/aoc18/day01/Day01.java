8f30c5a75e708e8cd2813ba68095fed1a9b09ecc
Christian Mesch <christianmesch@gmail.com>
2018-12-01 16:45:55 +0100

    Using set instead of list

--- aoc18/src/main/java/io/christianmesch/aoc18/day01/Day01.java
+++ aoc18/src/main/java/io/christianmesch/aoc18/day01/Day01.java
@@ -4,7 +4,9 @@ import io.christianmesch.aoc18.utils.InputUtils;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Day01 {
@@ -14,22 +16,20 @@ public class Day01 {
   public static void main(String... args) {
     Day01 day = new Day01();
     try {
-      day.puzzle1();
-      day.puzzle2();
+      System.out.println(day.puzzle1());
+      System.out.println(day.puzzle2());
     } catch (URISyntaxException | IOException e) {
       e.printStackTrace();
     }
   }
 
-  private void puzzle1() throws URISyntaxException, IOException {
-    int sum = InputUtils.read(INPUT_FILENAME).stream().mapToInt(Integer::valueOf).sum();
-
-    System.out.println(sum);
+  private int puzzle1() throws URISyntaxException, IOException {
+    return InputUtils.read(INPUT_FILENAME).stream().mapToInt(Integer::valueOf).sum();
   }
 
-  private void puzzle2() throws IOException, URISyntaxException {
+  private int puzzle2() throws IOException, URISyntaxException {
     List<Integer> input = InputUtils.read(INPUT_FILENAME).stream().map(Integer::valueOf).collect(Collectors.toList());
-    List<Integer> foundFrequencies = new ArrayList<>();
+    Set<Integer> foundFrequencies = new HashSet<>();
 
     int lastFrequency = 0;
     foundFrequencies.add(0);
@@ -37,12 +37,9 @@ public class Day01 {
       for (int change : input) {
         lastFrequency += change;
 
-        if (foundFrequencies.contains(lastFrequency)) {
-          System.out.println(lastFrequency);
-          return;
+        if (!foundFrequencies.add(lastFrequency)) {
+          return lastFrequency;
         }
-
-        foundFrequencies.add(lastFrequency);
       }
     }
   }
