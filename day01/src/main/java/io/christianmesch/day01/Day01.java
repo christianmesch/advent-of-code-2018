46868bac2444bb2ad3f8fcf83c8f8ce65e7b7977
Christian Mesch <christianmesch@gmail.com>
2018-12-01 15:03:51 +0100

    Day 1, puzzle 2

--- day01/src/main/java/io/christianmesch/day01/Day01.java
+++ day01/src/main/java/io/christianmesch/day01/Day01.java
@@ -5,26 +5,49 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day01 {
   public static void main(String... args) {
     Day01 day = new Day01();
     try {
       day.puzzle1();
+      day.puzzle2();
     } catch (URISyntaxException | IOException e) {
       e.printStackTrace();
     }
   }
 
-  private List<String> readInput(String filename) throws URISyntaxException, IOException {
-    Path inputPath = Paths.get(Day01.class.getResource(filename).toURI());
+  private List<String> readInput() throws URISyntaxException, IOException {
+    Path inputPath = Paths.get(Day01.class.getResource("/input.txt").toURI());
     return Files.readAllLines(inputPath);
   }
 
   private void puzzle1() throws URISyntaxException, IOException {
-    int sum = readInput("/input.txt").stream().mapToInt(Integer::valueOf).sum();
+    int sum = readInput().stream().mapToInt(Integer::valueOf).sum();
 
     System.out.println(sum);
+  }
+
+  private void puzzle2() throws IOException, URISyntaxException {
+    List<Integer> input = readInput().stream().map(Integer::valueOf).collect(Collectors.toList());
+    List<Integer> foundFrequencies = new ArrayList<>();
+
+    int lastFrequency = 0;
+    foundFrequencies.add(0);
+    while(true) {
+      for (int change : input) {
+        lastFrequency += change;
+
+        if (foundFrequencies.contains(lastFrequency)) {
+          System.out.println(lastFrequency);
+          return;
+        }
+
+        foundFrequencies.add(lastFrequency);
+      }
+    }
   }
 }
