69a920bcd695a416618e17f3c5974c3ed4b73613
Christian Mesch <christianmesch@gmail.com>
2018-12-05 17:16:40 +0100

    Day 4, part 2

--- aoc18/src/main/java/io/christianmesch/aoc18/day04/Day04.java
+++ aoc18/src/main/java/io/christianmesch/aoc18/day04/Day04.java
@@ -6,6 +6,7 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -21,12 +22,31 @@ public class Day04 {
     Day04 day = new Day04();
     try {
       System.out.println(day.part1());
+      System.out.println(day.part2());
     } catch (Exception e) {
       e.printStackTrace();
     }
   }
 
   private long part1() throws Exception {
+    Guard found = getGuards().stream()
+        .sorted((a, b) -> b.sleepsAt.size() - a.sleepsAt.size())
+        .findFirst()
+        .orElse(new Guard(-1));
+
+    return found.id * found.getMostFrequentSleepingEntry().getKey();
+  }
+
+  private long part2() throws Exception {
+    Guard found = getGuards().stream()
+        .sorted((a, b) -> b.getMostFrequentSleepingEntry().getValue().intValue() - a.getMostFrequentSleepingEntry().getValue().intValue())
+        .findFirst()
+        .orElse(new Guard(-1));
+
+    return found.id * found.getMostFrequentSleepingEntry().getKey();
+  }
+
+  private List<Guard> getGuards() throws Exception {
     List<LogEntry> logs = InputUtils.read(INPUT_FILENAME).stream()
         .map(PATTERN::matcher)
         .filter(Matcher::matches)
@@ -55,30 +75,31 @@ public class Day04 {
       }
     }
 
-    Guard found = guards.values().stream()
-        .sorted((a, b) -> b.sleepsAt.size() - a.sleepsAt.size())
-        .findFirst()
-        .orElse(new Guard(-1));
-
-    return found.id * found.getMostFrequentSleepingTime();
+    return guards.values().stream()
+        .filter(g -> !g.sleepsAt.isEmpty())
+        .collect(Collectors.toList());
   }
 
   private class Guard {
     public final int id;
     public final List<Integer> sleepsAt = new ArrayList<>();
+    public Entry<Integer, Long> mostFrequent = null;
 
     public Guard(int id) {
       this.id = id;
     }
 
-    public int getMostFrequentSleepingTime() {
-      return sleepsAt.stream()
-          .collect(Collectors.groupingBy(t -> t, Collectors.counting()))
-          .entrySet().stream()
-          .sorted((a, b) -> b.getValue().intValue() - a.getValue().intValue())
-          .findFirst()
-          .orElseThrow(RuntimeException::new)
-          .getKey();
+    public Entry<Integer, Long> getMostFrequentSleepingEntry() {
+      if (mostFrequent == null) {
+        mostFrequent = sleepsAt.stream()
+            .collect(Collectors.groupingBy(t -> t, Collectors.counting()))
+            .entrySet().stream()
+            .sorted((a, b) -> b.getValue().intValue() - a.getValue().intValue())
+            .findFirst()
+            .orElseThrow(RuntimeException::new);
+      }
+
+      return mostFrequent;
     }
   }
 
