297455379a2d8d841b111ee61e96bdb286ee1a00
Christian Mesch <christianmesch@gmail.com>
2018-12-07 13:38:07 +0100

    Day 6, part 2

--- aoc18/src/main/java/io/christianmesch/aoc18/day06/Day06.java
+++ aoc18/src/main/java/io/christianmesch/aoc18/day06/Day06.java
@@ -8,11 +8,13 @@ import java.util.stream.Collectors;
 
 public class Day06 {
   private static final String INPUT_FILENAME = "/day6.txt";
+  private static final int MAX_DISTANCE = 10000;
 
   public static void main(String... args) {
     Day06 day = new Day06();
     try {
       System.out.println(day.part1());
+      System.out.println(day.part2());
     } catch (Exception e) {
       e.printStackTrace();
     }
@@ -78,6 +80,43 @@ public class Day06 {
         .stream()
         .max(Comparator.naturalOrder())
         .orElse(-1L);
+  }
+
+  private long part2() throws Exception {
+    List<Coordinate> coordinates = InputUtils.read(INPUT_FILENAME).stream()
+        .map(s -> s.split(", "))
+        .map(s -> new Coordinate(Integer.valueOf(s[0]), Integer.valueOf(s[1])))
+        .collect(Collectors.toList());
+
+    int xMin = coordinates.get(0).x;
+    int xMax = xMin;
+    int yMin = coordinates.get(0).y;
+    int yMax = yMin;
+
+    for(Coordinate c : coordinates) {
+      xMin = Integer.min(xMin, c.x);
+      xMax = Integer.max(xMax, c.x);
+      yMin = Integer.min(yMin, c.y);
+      yMax = Integer.max(yMax, c.y);
+    }
+
+    List<Coordinate> allCoordinates = new ArrayList<>();
+    for (int x = xMin; x <= xMax; x++) {
+      for (int y = yMin; y < yMax; y++) {
+        allCoordinates.add(new Coordinate(x, y));
+      }
+    }
+
+    for (Coordinate c1 : coordinates) {
+      for (Coordinate c2 : allCoordinates) {
+        c2.distance += distance(c1, c2);
+      }
+    }
+
+    return allCoordinates.stream()
+        .filter(c -> c.distance < MAX_DISTANCE)
+        .map(c -> c.distance)
+        .count();
   }
 
   private int distance(Coordinate c1, Coordinate c2) {
